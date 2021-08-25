49b986a3320f2582d0891793432d74622f819761
Santosh Pingali <31937373+santoshp1995@users.noreply.github.com>
2021-08-25 18:34:31 -0500

    Add files via upload

---
+++ Animation/TimerController.cpp
@@ -0,0 +1,83 @@
+#include "TimerController.h"
+
+namespace Azul
+{
+	TimerController::TimerController(AnimTime Delta, AnimTime Min, AnimTime Max)
+		: tCurr(AnimTime::Duration::ZERO),
+		tDelta(Delta),
+		tMax(Max),
+		tMin(Min),
+		isPaused(false)
+	{
+	}
+
+	void TimerController::Update()
+	{
+		if (!isPaused)
+		{
+			tCurr += tDelta;
+
+			// protection for time values for looping
+			if (tCurr < tMin)
+			{
+				tCurr = tMax;
+			}
+			else if (tCurr > tMax)
+			{
+				tCurr = tMin;
+			}
+			else
+			{
+				// do nothing
+			}
+		}
+	}
+
+	void TimerController::SetCurrTime(const AnimTime time)
+	{
+		this->tCurr = time;
+	}
+
+	AnimTime TimerController::GetCurrTime() const
+	{
+		return this->tCurr;
+	}
+
+	void TimerController::SetDeltaTime(const AnimTime time)
+	{
+		this->tDelta = time;
+	}
+
+	AnimTime TimerController::GetDeltaTime() const
+	{
+		return this->tDelta;
+	}
+
+	void TimerController::SetMaxTime(const AnimTime time)
+	{
+		this->tMax = time;
+	}
+
+	AnimTime TimerController::GetMaxTime() const
+	{
+		return this->tMax;
+	}
+
+	void TimerController::SetMinTime(const AnimTime time)
+	{
+		this->tMin = time;
+	}
+
+	AnimTime TimerController::GetMinTime() const
+	{
+		return this->tMin;
+	}
+
+	void TimerController::SetPause(const bool b)
+	{
+
+		this->isPaused = b;
+
+	}
+
+}
---
+++ Animation/TimerController.h
@@ -0,0 +1,43 @@
+#ifndef TIMER_CONTROLLER_H
+#define TIMER_CONTROLLER_H
+
+#include "AnimTimer.h"
+
+namespace Azul
+{
+	class TimerController
+	{
+	public:
+		TimerController(AnimTime Delta, AnimTime Min, AnimTime Max);
+
+		TimerController() = delete;
+		TimerController(const TimerController&) = delete;
+		TimerController& operator = (const TimerController&) = delete;
+		~TimerController() = default;
+
+		void Update();
+
+		// Accessors
+		void SetCurrTime(const AnimTime time);
+		AnimTime GetCurrTime() const;
+		void SetDeltaTime(const AnimTime time);
+		AnimTime GetDeltaTime() const;
+		void SetMaxTime(const AnimTime time);
+		AnimTime GetMaxTime() const;
+		void SetMinTime(const AnimTime time);
+		AnimTime GetMinTime() const;
+
+		// functions
+		void SetPause(const bool b);
+
+	private:
+		AnimTime tCurr;
+		AnimTime tDelta;
+		AnimTime tMax;
+		AnimTime tMin;
+
+		bool isPaused;
+	};
+}
+
+#endif
